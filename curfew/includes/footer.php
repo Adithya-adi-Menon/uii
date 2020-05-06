c11e815fe4784a49b4d259282ad35b2b0f559ebb
adithya menon <adithyamenons17@gmail.com>
2020-05-06 13:03:51 +0530

    first commit

---
+++ curfew/includes/footer.php
@@ -0,0 +1,10 @@
+<footer class="footer-area section-gap">
+        <div class="container">
+
+            <!-- <div class="footer-bottom row align-items-center text-center text-lg-left">
+                <p class="footer-text m-0 col-lg-8 col-md-12">
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | 
+</p>
+            </div> -->
+        </div>
+    </footer>
